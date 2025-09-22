29c17cdbd52fb889939aff897a35b2a0d50e62ad
Ykk2001 <160621076+Ykk2001@users.noreply.github.com>
2025-09-22 10:37:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,6 +4,26 @@ document.body.appendChild(getSumBtn);
 
 const getSum = () => {
 //Add your code here
+let priceAll=document.querySelectorAll('.price')
+console.log(priceAll);
+
+	//
+	let total=0;
+	priceAll.forEach((cell)=>{
+		 total=total+parseFloat(cell.textContent)
+	})
+	
+	// Create a new row for the total
+    const table = document.getElementById("groceryTable");
+    const totalRow = document.createElement("tr");
+    totalRow.classList.add("total-row");
+
+	const totalCell = document.createElement("td");
+    totalCell.setAttribute("colspan", "2");
+    totalCell.textContent = "Total Price: " + total;
+
+    totalRow.appendChild(totalCell);
+    table.appendChild(totalRow);
   
 };
 
